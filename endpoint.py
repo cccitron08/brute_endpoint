583eff311f5ca6a1ff0abbd28cbf8c7aacc38951
cccitron <cccitron08@gmail.com>
2025-11-30 22:38:42 +0100

    Finished limit fuzz function

--- endpoint.py
+++ endpoint.py
@@ -56,10 +56,40 @@ def limitless_brute_force(endpoint):
         pass
     return False
 
+def limit_send_endpoint(endpoint):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((hostname, port))
+
+    client_socket.sendall(endpoint.encode())
+    response = client_socket.recv(1024).decode()
+    if not f"name '{endpoint}' is not defined" in response:
+        return True
+    
+    return False
+
+def limit_fuzz_endpoint():
+    fuzz_again = 'y'
+    while fuzz_again != 'n':
+        with open(endpoints_wordlist, 'rb') as f:
+            endpoints = f.read()
+
+            for endpoint in endpoints.decode(errors="ignore").splitlines():
+                if limit_send_endpoint(endpoint):
+                    print(f"Found endpoint: '{endpoint}'")
+                    fuzz_again = input("Try to find another endpoint? (y/n)")
+                    if fuzz_again.lower() == 'y':
+                        continue
+                    elif fuzz_again.lower() == 'n':
+                        break
+                    else:
+                        print("Wrong input (y/n)")
+                else:
+                    print(f"Endpoint '{endpoint}' was incorrect, reconnecting!")
+
 def limit_send_password():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket = client_socket.connect((hostname, port))
-
+    client_socket.connect((hostname, port))
+    client_socket.sendall()
 
 def limit_brute_force():
     with open(passwords_wordlist, 'rb') as f:
@@ -72,6 +102,7 @@ def limit_brute_force():
 
         
 def main():
+    """
     print("There are 2 versions, limitless and limit")
     print("Use limitless if there's no brute force protection (max ammount of tries)")
     print("use limit if there's a limit of how much tries you get")
@@ -81,12 +112,12 @@ def main():
             limitless_fuzz_endpoints()
             break
         elif version.lower() == "limit":
-            limit_connect_endpoint()
+            limit_brute_force()
             break
         else:
             print("Wrong choice (limitless/limit)")
-            
-            
+    """    
+    limit_fuzz_endpoint()        
 
 if __name__ == "__main__":
     main()
